3c981cb0a6d2005191cf548698c63286fa8f6f76
Khan Kibria <khan.kibria@gmail.com>
2024-03-21 17:11:13 -0700

    Function working

--- src/gitize/external.py
+++ src/gitize/external.py
@@ -1,8 +1,25 @@
+import json
+from pathlib import Path
 import subprocess
 from urllib.request import urlopen
+from prj_gen.userinput import get_choices_from_dict
 
-def get_licenses(): 
-    return urlopen('https://api.github.com/licenses').read()
+def get_license(path): 
+    KEY = "key"
+    with urlopen('https://api.github.com/licenses') as url:
+        lics = json.load(url)
+    ch = {}
+    urls = {}  
+    for i in lics:
+        key = i[KEY]
+        ch[key] = i["name"]
+        urls[key] = i["url"]
+    r = get_choices_from_dict(key="licenses", ch=ch, df=lics[0][KEY], pr="licenses")
+    with urlopen(urls[r[KEY]]) as url:
+        lic = json.load(url)
+    dst = Path(path).joinpath("LICENSE.txt")
+    with dst.open("w") as f:
+        f.write(lic["body"])
 
 def cmds(path):
     return {
