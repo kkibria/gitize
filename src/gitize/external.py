79ec0f75e2382adc4067d22d99015a8a949f4a6a
Khan Kibria <khan.kibria@gmail.com>
2024-03-23 19:00:32 -0700

    moved license writing function to library

--- src/gitize/external.py
+++ src/gitize/external.py
@@ -1,30 +1,8 @@
-import json
-from pathlib import Path
 import subprocess
-from urllib.request import urlopen
-from prj_gen.userinput import get_choices_from_list
-from requests_cache import CachedSession
+from prj_gen.common import get_license
 
-KEY = "key"
-
-# Grab license from github
-def get_license(path): 
-    session = CachedSession()
-    response = session.get('https://api.github.com/licenses')
-    lics = response.json()
-    if len(lics) < 1:
-        return
-    names = []
-    urls = []  
-    for i in lics:
-        names.append(i["name"])
-        urls.append(i["url"])
-    i_lic = get_choices_from_list(key="License", ch=names, df=0, pr="License")
-    with urlopen(urls[i_lic[KEY]]) as url:
-        lic = json.load(url)
-    dst = Path(path).joinpath("LICENSE.txt")
-    with dst.open("w") as f:
-        f.write(lic["body"])
+def write_license(path):
+    get_license(path, 'gitize')
 
 def cmds(path):
     return {
